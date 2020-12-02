854d4f3c7d9742a5973bf2dc0e22646a658e040c
Ubuntu <ubuntu@ip-172-31-0-34.ec2.internal>
2020-12-02 23:00:27 +0000

    finishing up Rest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def index
     @users = User.all
@@ -35,10 +36,15 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       flash.now[:success] = "Profile Updated"
       redirect_to @user
-      #Handle successful uodate
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
 private
@@ -62,4 +68,8 @@ end
       redirect_to(root_url) unless current_user?(@user)
     end
     
+    #confirm admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
