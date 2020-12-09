468f28dabc2bd17a2d6ed69fd4a9bfa218f67894
RameshHolly <rameshbaglung90@gmail.com>
2020-12-10 09:57:02 +1100

    Add react stickies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 import "bootstrap"// Support component names relative to this directory:
+import '../stylesheets/application';
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,27 +2,10 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import { default as Stickies } from './components/Stickies';
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+ export {
+   Stickies
+ };
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+ export default Stickies;
